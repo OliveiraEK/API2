d7d3c3e150c8a1e7bbeb2c415474a27bfca49d81
ERIK OLIVEIRA SANTOS <erik.osantos@senacsp.edu.br>
2022-11-30 11:50:39 -0300

    first commit

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express"
+import { Phrase } from "../models/Phrase"
+
+export const ping = (req:Request, res:Response) =>{
+    res.json({pong:true})
+}
+export const random = (req:Request, res:Response) =>{
+    let nRand: number = Math.floor (Math.random() * 10)
+    res.json({number: nRand})
+}
+export const nome = (req:Request,res:Response) =>{
+    let nome: string = req.params.nome
+    res.json({nome: `Você enviou o nome ${nome}`})
+}
+
+export const createPhrase =  async(req: Request, res: Response) =>{
+    let {author, txt} = req.body
+    let newPhrase = await Phrase.create({author, txt})
+    res.json({id: newPhrase.id, author , txt})
+}
+
+export const listPhrases = async(req:Request, res: Response) =>{
+    let list = await Phrase.findAll()
+    res.json({list})
+
+}
+export const getPhrase = async(req:Request, res:Response) =>{
+    let { id } = req.params
+    let phrase = await Phrase.findByPk(id)
+    if(phrase){
+        res.json({phrase})
+    }else{
+        res.json({error: 'Frase não existe'})
+    }
+}
